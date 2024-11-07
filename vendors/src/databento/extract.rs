f31b5b4f227be67f53a249f5ac27feae19326719
anthony baxter <anthony_baxter@student.uml.edu>
2024-11-06 20:02:25 -0500

    UPD: Updated databento vendor batch loading process.

--- vendors/src/databento/extract.rs
+++ vendors/src/databento/extract.rs
@@ -37,59 +37,86 @@ pub async fn read_dbn_file(
     Ok((decoder, map))
 }
 
-/// Read directory created by dbn batch job.
-pub async fn read_dbn_batch_dir(
-    dir_path: PathBuf,
-) -> Result<(
-    AsyncDbnDecoder<ZstdDecoder<BufReader<File>>>,
-    HashMap<String, String>,
-)> {
-    // List files in batch dir
-    let mut files = Vec::new();
+pub async fn read_dbn_batch_dir(dir_path: &PathBuf) -> Result<Vec<PathBuf>> {
+    // List files in directory with .zst extension
+    let mut zstd_files = Vec::new();
+
     for entry in WalkDir::new(dir_path)
         .into_iter()
         .filter_map(|e| e.ok())
         .filter(|e| e.file_type().is_file())
     {
-        files.push(entry.path().to_path_buf());
-    }
-
-    let mut symbol_map_hash = HashMap::new();
-    let mut decoder = None;
-
-    // Decode Files
-    for downloaded_file in files {
-        if let Some(extension) = downloaded_file.extension() {
+        if let Some(extension) = entry.path().extension() {
             if extension == "zst" {
-                let current_decoder =
-                    AsyncDbnDecoder::with_zstd(File::open(&downloaded_file).await?).await?;
-
-                // Extract Symbol Map
-                let metadata = current_decoder.metadata();
-                let map = symbol_map(&metadata)?;
-
-                // Merge the symbol map into the global map
-                for (id, ticker) in map {
-                    symbol_map_hash.insert(id, ticker.clone());
-                }
-
-                // Set decoder to the first file found
-                if decoder.is_none() {
-                    decoder = Some(current_decoder);
-                }
+                zstd_files.push(entry.path().to_path_buf());
             }
         }
     }
 
-    // Ensure a decoder was found
-    if let Some(decoder) = decoder {
-        Ok((decoder, symbol_map_hash))
+    if !zstd_files.is_empty() {
+        Ok(zstd_files)
     } else {
         Err(crate::Error::CustomError(
             "No Zstd compressed files found".to_string(),
         ))
     }
 }
+
+/// Read directory created by dbn batch job.
+// pub async fn read_dbn_batch_dir(
+//     dir_path: PathBuf,
+// ) -> Result<(
+//     AsyncDbnDecoder<ZstdDecoder<BufReader<File>>>,
+//     HashMap<String, String>,
+// )> {
+//     // List files in batch dir
+//     let mut files = Vec::new();
+//     for entry in WalkDir::new(dir_path)
+//         .into_iter()
+//         .filter_map(|e| e.ok())
+//         .filter(|e| e.file_type().is_file())
+//     {
+//         files.push(entry.path().to_path_buf());
+//     }
+//
+//     let mut symbol_map_hash = HashMap::new();
+//     let mut decoder = None;
+//
+//     // Decode Files
+//     for downloaded_file in files {
+//         println!("{:?}", downloaded_file);
+//
+//         if let Some(extension) = downloaded_file.extension() {
+//             if extension == "zst" {
+//                 let current_decoder =
+//                     AsyncDbnDecoder::with_zstd(File::open(&downloaded_file).await?).await?;
+//
+//                 // Extract Symbol Map
+//                 let metadata = current_decoder.metadata();
+//                 let map = symbol_map(&metadata)?;
+//
+//                 // Merge the symbol map into the global map
+//                 for (id, ticker) in map {
+//                     symbol_map_hash.insert(id, ticker.clone());
+//                 }
+//
+//                 // Set decoder to the first file found
+//                 if decoder.is_none() {
+//                     decoder = Some(current_decoder);
+//                 }
+//             }
+//         }
+//     }
+//
+//     // Ensure a decoder was found
+//     if let Some(decoder) = decoder {
+//         Ok((decoder, symbol_map_hash))
+//     } else {
+//         Err(crate::Error::CustomError(
+//             "No Zstd compressed files found".to_string(),
+//         ))
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
@@ -114,6 +141,7 @@ mod tests {
     }
 
     #[tokio::test]
+    #[ignore]
     async fn test_read_dbn_stream_file() -> Result<()> {
         let file_path = setup(&PathBuf::from("tests/data/databento"))?;
 
@@ -137,16 +165,19 @@ mod tests {
         let dir_path = setup(&PathBuf::from("tests/data/databento/batch"))?;
 
         // Test
-        let (mut decoder, map) = read_dbn_batch_dir(dir_path).await?;
+        let files = read_dbn_batch_dir(&dir_path).await?;
+        for file in files {
+            let (mut decoder, map) = read_dbn_file(file).await?;
 
-        let mut records = Vec::new();
-        while let Some(record) = decoder.decode_record::<dbn::Mbp1Msg>().await? {
-            records.push(record.clone());
+            let mut records = Vec::new();
+            while let Some(record) = decoder.decode_record::<dbn::Mbp1Msg>().await? {
+                records.push(record.clone());
+            }
+
+            // Validate
+            assert!(records.len() > 0);
+            assert!(!map.is_empty(), "The map should not be empty");
         }
-
-        // Validate
-        assert!(records.len() > 0);
-        assert!(!map.is_empty(), "The map should not be empty");
 
         Ok(())
     }
